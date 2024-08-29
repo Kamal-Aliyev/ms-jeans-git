8e860bbf10f58c2404c8c32a7850d0dc79f8ffdc
Kamal-Aliyev <kamalliyev39@gmail.com>
2024-08-29 15:43:21 +0400

    BHS-42 : Some texts added

--- src/az/ms/jeans/git/Main.java
+++ src/az/ms/jeans/git/Main.java
@@ -3,5 +3,8 @@ package az.ms.jeans.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World !");
+        System.out.println("Kamal Xuramano cox sevir");
+        String love = "Endless love for you my love ";
+        System.out.printf("I m always here for you, %s",love);
     }
 }
