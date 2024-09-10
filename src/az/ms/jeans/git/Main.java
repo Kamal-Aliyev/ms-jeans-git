b90d21720aca4dc8f493de782f72a08aaa6580da
Kamal-Aliyev <kamalliyev39@gmail.com>
2024-09-10 15:01:36 +0400

    BSA-44 : new text lane added

--- src/az/ms/jeans/git/Main.java
+++ src/az/ms/jeans/git/Main.java
@@ -3,9 +3,10 @@ package az.ms.jeans.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World !");
-        System.out.println("Kamal Xuramani cox sevir");
+        System.out.println("dreamland");
         String love = "Endless love for you my love ";
         System.out.printf("I m always here for you, %s",love);
         System.out.println("Drake COLLAB");
+        System.out.println("Kaseda");
     }
 }
