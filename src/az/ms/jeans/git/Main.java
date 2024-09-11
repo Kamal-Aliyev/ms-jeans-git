a9f4b411b13bbd89cbc1b888fde39625d9e43c4a
Kamal-Aliyev <kamalliyev39@gmail.com>
2024-09-11 21:10:48 +0400

    BSA-46 : new method added to Bank class

--- src/az/ms/jeans/git/Main.java
+++ src/az/ms/jeans/git/Main.java
@@ -1,5 +1,7 @@
 package az.ms.jeans.git;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World !");
@@ -8,5 +10,10 @@ public class Main {
         System.out.printf("I m always here for you, %s",love);
         System.out.println("Drake COLLAB");
         System.out.println("Kaseda");
+
+
+
+
     }
+
 }
